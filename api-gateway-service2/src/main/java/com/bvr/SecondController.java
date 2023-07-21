fe1959df87f5a3e349998a58861a40f0dd9d7189
Vishwanath Rao <vishymails@gmail.com>
2023-07-21 16:25:14 +0530

    Add files via upload

---
+++ api-gateway-service2/src/main/java/com/bvr/SecondController.java
@@ -0,0 +1,19 @@
+package com.bvr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class SecondController {
+
+	public SecondController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@GetMapping("/message")
+	public String test() {
+		return "Hello api demo called in Second service";
+	}
+}
